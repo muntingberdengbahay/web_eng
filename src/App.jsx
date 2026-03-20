daa4188aa748688ddc1bd8cc66db22e634cc9fff
Clark Mac <emailclarkmaclang@gmail.com>
2026-03-21 00:52:15 +0800

    Created About and Admissions Pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Work from "./pages/Work";
 import Services from "./pages/Services";
+import BeABulSUan from "./pages/BeABulSUan";
+import AboutBulSU from "./pages/AboutBulSU";
+import Admissions from "./pages/Admissions";
+import About from "./pages/About";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/be-a-bulsuan" element={<BeABulSUan />} />
       <Route path="/services" element={<Services />} />
       <Route path="/work" element={<Work />} />
+      <Route path="/about-bulsu" element={<AboutBulSU />} />
+      <Route path="/admissions" element={<Admissions />} />
+      <Route path="/about" element={<About />} />
+
     </Routes>
   );
 }
